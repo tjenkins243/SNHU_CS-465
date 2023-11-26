4fd66a6f8865f8cb819d02551072ff1aed9979e4
Thomas Jenkins <thomas.jenkins@snhu.edu>
2023-11-26 02:25:51 -0600

    updating minor changes

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const controller = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', controller.index) 
+router.get('/', controller.index); 
 
 module.exports = router;

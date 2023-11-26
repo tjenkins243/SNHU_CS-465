9f0e0a0a9653317d6dd27b8f6b2a426e88dfacb4
Thomas Jenkins <thomas.jenkins@snhu.edu>
2023-11-26 03:12:03 -0600

    Small fixes as refollow past walkthroughs

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
+
 hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,12 +1,9 @@
 const fs = require('fs');
 const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
-/* GET traveler view */
 const travel = (req, res) => {
-    let pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel', {
-        title: pageTitle,
-        trips});
+    pageTitle = process.env.npm_package_description + " - Travel";
+    res.render('travel', {title: pageTitle,trips});
 };
 
 module.exports = {
